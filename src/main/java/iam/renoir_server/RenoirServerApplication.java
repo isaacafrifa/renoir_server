1ec31f7b96ebbeae919e7885cf6b2ff30f98bceb
Yaw Afrifa <isaac.afrifa3@yahoo.com>
2022-05-15 12:34:54 +0000

    Initial commit

---
+++ src/main/java/iam/renoir_server/RenoirServerApplication.java
@@ -0,0 +1,13 @@
+package iam.renoir_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenoirServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RenoirServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iam/renoir_server/RenoirServerApplicationTests.java
@@ -0,0 +1,13 @@
+package iam.renoir_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenoirServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
