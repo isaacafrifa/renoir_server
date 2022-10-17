bbc3e9d590d8321b2ef7b2f24f578928baf7e75e
Afrifa <isaac.afrifa3@yahoo.com>
2022-10-17 20:16:37 +0300

    FEAT: Implement basic API Key functionality
    Authorize all incoming requests with API key filter

--- src/main/java/com/iam/config/SecurityConfig.java
+++ src/main/java/com/iam/config/SecurityConfig.java
@@ -1,15 +1,12 @@
 package com.iam.config;
 
 
-import com.iam.auth.APIKeyAuthFilter;
+import com.iam.auth.ApiKeyAuthenticationFilter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,9 +14,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
 @EnableWebSecurity
@@ -38,35 +34,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder(10);
     }
 
+    @Bean
+    public ApiKeyAuthenticationFilter filterBean(){
+        return new ApiKeyAuthenticationFilter();
+    }
+
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        APIKeyAuthFilter filter = new APIKeyAuthFilter(principalRequestHeader);
-        filter.setAuthenticationManager(new AuthenticationManager() {
-            @Override
-            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-                String principal = (String) authentication.getPrincipal();
-                if (!principalRequestValue.equals(principal)) {
-                    log.error("API Key[{}] was not found or not the expected value", principal);
-                    throw new BadCredentialsException("Bad credentials");
-//                    throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,"you can't consume this service , Please validate your apikeys");
-
-//                    HttpServletResponse httpResponse = (HttpServletResponse) response;
-//                    httpResponse.setStatus(401);
-//                    httpResponse.getWriter().write("Invalid API Key");
-                }
-//                ApiKeyAuthenticationToken apiToken = new ApiKeyAuthenticationToken(apiKey, AuthorityUtils.NO_AUTHORITIES);
-//                SecurityContextHolder.getContext().setAuthentication(apiToken);
-                authentication.setAuthenticated(true);
-                return authentication;
-            }
-        });
 
         httpSecurity.
                 antMatcher("/api/**")
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .addFilter(filter)
+                .addFilterBefore(filterBean(), UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .anyRequest().authenticated();
 
