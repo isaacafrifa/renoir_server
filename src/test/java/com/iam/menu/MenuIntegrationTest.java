7c1d43c4979d71a6c271b5d4315a1539e1608a2c
Afrifa <isaac.afrifa3@yahoo.com>
2023-05-11 00:54:46 +0300

    test🧪: implement IT for all endpoints

--- src/test/java/com/iam/menu/MenuIntegrationTest.java
+++ src/test/java/com/iam/menu/MenuIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.iam.menu;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.iam.RenoirServerApplication;
 import org.json.JSONException;
 import org.junit.jupiter.api.DisplayName;
@@ -12,9 +13,13 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.jdbc.Sql;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.UUID;
 
 
 @SpringBootTest(classes = RenoirServerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -26,6 +31,9 @@ public class MenuIntegrationTest extends AbstractContainerBaseTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
+    @Autowired
+    ObjectMapper objectMapper;
+
     private String getRootUrlWithPort() {
         return "http://localhost:" + port + "/api/v1/menus";
     }
@@ -34,10 +42,10 @@ public class MenuIntegrationTest extends AbstractContainerBaseTest {
     @Sql({"/data.sql"})
     @Test
     void testAllMenus() throws JSONException {
-        String expected= """
+        String expected = """
                 {"totalElements": 4}
                 """;
-        ResponseEntity<String> actual  = restTemplate.getForEntity(getRootUrlWithPort(), String.class);
+        ResponseEntity<String> actual = restTemplate.getForEntity(getRootUrlWithPort(), String.class);
         JSONAssert.assertEquals(expected, actual.getBody(), false);
     }
 
@@ -53,14 +61,64 @@ public class MenuIntegrationTest extends AbstractContainerBaseTest {
                 "comment":"test comment"
                  }
                 """.formatted(uuid);
-        ResponseEntity<String> actual = restTemplate.getForEntity(getRootUrlWithPort()+"/"+uuid,String.class);
+        ResponseEntity<String> actual = restTemplate.getForEntity(getRootUrlWithPort() + "/" + uuid, String.class);
         JSONAssert.assertEquals(expected, actual.getBody(), true);
     }
 
     @Test
-    void createMenu()  {
-        MenuDto menuDto= new MenuDto(null,"rice","test", BigDecimal.TEN,Category.MAIN_DISH,null);
-        ResponseEntity<MenuDto> actual = restTemplate.postForEntity(getRootUrlWithPort(),menuDto, MenuDto.class);
+    void createMenu() {
+        MenuDto menuDto = new MenuDto(null, "rice", "test", BigDecimal.TEN, Category.MAIN_DISH, null);
+        ResponseEntity<MenuDto> actual = restTemplate.postForEntity(getRootUrlWithPort(), menuDto, MenuDto.class);
         assertEquals(201, actual.getStatusCode().value());
     }
+
+    @Test
+    void updateMenu() {
+        // Create a menu item to be updated
+        MenuDto menuDto = new MenuDto(null, "rice", "test", BigDecimal.TEN, Category.MAIN_DISH, null);
+        ResponseEntity<MenuDto> createdResponse = restTemplate.postForEntity(getRootUrlWithPort(), menuDto, MenuDto.class);
+        assertEquals(201, createdResponse.getStatusCode().value());
+
+        // Retrieve the created menu item's ID
+        UUID menuId = Objects.requireNonNull(createdResponse.getBody()).getId();
+
+        // Prepare the updated menu item data
+        MenuDto updatedMenuDto = new MenuDto(menuId, "updated rice", "updated description", BigDecimal.valueOf(15.0), Category.MAIN_DISH, "updated comment");
+
+        // Send an HTTP PUT request to update the menu item
+        restTemplate.put(getRootUrlWithPort() + "/" + menuId, updatedMenuDto);
+
+        // Retrieve the updated menu item
+        ResponseEntity<MenuDto> updatedResponse = restTemplate.getForEntity(getRootUrlWithPort() + "/" + menuId, MenuDto.class);
+
+        // Verify that the update was successful
+        assertAll(
+                () -> assertEquals(200, updatedResponse.getStatusCode().value()),
+                () -> assertEquals(updatedMenuDto.getName(), updatedResponse.getBody().getName()),
+                () -> assertEquals(updatedMenuDto.getDescription(), updatedResponse.getBody().getDescription()),
+                () -> assertEquals(updatedMenuDto.getCategory(), updatedResponse.getBody().getCategory())
+        );
+    }
+
+    @Test
+    void deleteMenu() {
+        // Create a menu item to be deleted
+        MenuDto menuDto = new MenuDto(null, "chicken wings", "fried foods", BigDecimal.valueOf(9.0), Category.SIDES, null);
+        ResponseEntity<MenuDto> createdResponse = restTemplate.postForEntity(getRootUrlWithPort(), menuDto, MenuDto.class);
+        assertEquals(201, createdResponse.getStatusCode().value());
+
+        // Retrieve the created menu item's ID
+        UUID menuId = Objects.requireNonNull(createdResponse.getBody()).getId();
+
+        // Send an HTTP DELETE request to delete the menu item
+        restTemplate.delete(getRootUrlWithPort() + "/" + menuId);
+
+        // Try to retrieve the deleted menu item
+        ResponseEntity<MenuDto> deletedResponse = restTemplate.getForEntity(getRootUrlWithPort() + "/" + menuId, MenuDto.class);
+
+        // Verify that the menu item is no longer accessible
+        assertEquals(404, deletedResponse.getStatusCode().value());
+    }
+
+
 }
