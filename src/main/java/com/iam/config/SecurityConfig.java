0fff20ea85d24f1137c19037205e86b30c6e36b3
Afrifa <isaac.afrifa3@yahoo.com>
2023-05-20 02:33:59 +0300

    feat(security): add basic auth for starters

--- src/main/java/com/iam/config/SecurityConfig.java
+++ src/main/java/com/iam/config/SecurityConfig.java
@@ -1,55 +1,34 @@
 package com.iam.config;
 
 
-import com.iam.auth.ApiKeyAuthenticationFilter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true) //this LOC enables the @PreAuthorize annotation.
-@Order(1)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @Slf4j
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Value("${auth-token-header-name}")
-    private String principalRequestHeader;
-
-    @Value("${auth-token}")
-    private String principalRequestValue;
+public class SecurityConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoderBean() {
-        return new BCryptPasswordEncoder(10);
+    protected SecurityFilterChain apiKeyFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+       return httpSecurity
+               .csrf(AbstractHttpConfigurer::disable)
+               .sessionManagement(sessManagement -> sessManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+               .authorizeHttpRequests(auth ->
+                       auth.anyRequest().authenticated())
+               .httpBasic(Customizer.withDefaults()) //TODO change to apiKey auth
+               .build();
     }
 
-    @Bean
-    public ApiKeyAuthenticationFilter filterBean(){
-        return new ApiKeyAuthenticationFilter();
-    }
-
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-
-        httpSecurity.
-                antMatcher("/api/**")
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .addFilterBefore(filterBean(), UsernamePasswordAuthenticationFilter.class)
-                .authorizeRequests()
-                .anyRequest().authenticated();
-
-    }
 }
