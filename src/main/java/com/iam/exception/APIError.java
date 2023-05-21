3402594b430b5f326401611f8a764e8abefb294b
Afrifa <isaac.afrifa3@yahoo.com>
2023-05-21 04:44:59 +0300

    refactor(api-error): change apiError to record

--- src/main/java/com/iam/exception/APIError.java
+++ src/main/java/com/iam/exception/APIError.java
@@ -1,18 +1,12 @@
 package com.iam.exception;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class APIError {
-    private String message;
-    private String path;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime timeStamp;
+public record APIError (
+        String message,
+        String path,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+        LocalDateTime timeStamp){
 }
