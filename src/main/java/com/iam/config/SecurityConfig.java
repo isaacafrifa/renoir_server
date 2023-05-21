879885bcb2caa9678023288662b805e3f42d1fbd
Afrifa <isaac.afrifa3@yahoo.com>
2023-05-21 04:39:27 +0300

    feat(security): implement api key auth

--- src/main/java/com/iam/config/SecurityConfig.java
+++ src/main/java/com/iam/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.iam.config;
 
-
+import com.iam.auth.ApiKeyAuthFilter;
+import com.iam.auth.CustomAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,20 +18,29 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(securedEnabled = true)
 @Slf4j
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final CustomAuthenticationEntryPoint entryPoint;
+    private final AppConfig appConfig;
 
     @Bean
-    protected SecurityFilterChain apiKeyFilterChain(HttpSecurity httpSecurity) throws Exception {
-
-       return httpSecurity
-               .csrf(AbstractHttpConfigurer::disable)
-               .sessionManagement(sessManagement -> sessManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-               .authorizeHttpRequests(auth ->
-                       auth.anyRequest().authenticated())
-               .httpBasic(Customizer.withDefaults()) //TODO change to apiKey auth
-               .build();
+    public AuthenticationManager authManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
+    @Bean
+    protected SecurityFilterChain apiKeyFilterChain(HttpSecurity httpSecurity, AuthenticationManager authManager) throws Exception {
+
+        ApiKeyAuthFilter apiKeyAuthFilter = new ApiKeyAuthFilter(authManager, appConfig);
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessManagement -> sessManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().authenticated())
+                .addFilter(apiKeyAuthFilter)
+                .exceptionHandling(exceptionHandler -> exceptionHandler.authenticationEntryPoint(entryPoint))
+                .build();
+    }
 }
